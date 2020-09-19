c586e629555f68b2e032f45334c34cd56b374037
ksimone <ksworthy@windstream.net>
2020-09-18 23:16:19 -0400

    new changes to watchlist.handlebars

--- public/js/watchlist.js
+++ public/js/watchlist.js
@@ -1,11 +1,11 @@
-$(() => {
-  $(".j-heart").on("click", function(event) {
-    event.stopPropagation();
+// $(() => {
+//   $(".j-heart").on("click", function(event) {
+//     event.stopPropagation();
 
-    const movieId = $(this)
-      .parent()
-      .attr("data-id");
-    $(this).toggleClass("is-active");
-    console.log("clicked " + movieId);
-  });
-});
+//     const movieId = $(this)
+//       .parent()
+//       .attr("data-id");
+//     $(this).toggleClass("is-active");
+//     console.log("clicked " + movieId);
+//   });
+// });
