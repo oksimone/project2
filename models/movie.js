8392443953c675ccf3aa8125b14dfb0c7d3ab159
Jordan Triplett <JordanT111596@gmail.com>
2020-09-21 12:11:16 -0400

    Removed allowNulls

--- models/movie.js
+++ models/movie.js
@@ -2,31 +2,26 @@ module.exports = function(sequelize, DataTypes) {
   const Movie = sequelize.define("Movie", {
     // The title will be grabbed from the api's original_title
     title: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.STRING
     },
     // The id will be grabbed from the api's id for the movie
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true
     },
     //This determines if the movie has been watched or not
     hasWatched: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
       default: false
     },
     //This determines if the movie has been added to the watch list
     onPlaylist: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
       default: false
     },
     //This determines if the movie has been added to favorites
     isFavorite: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
       default: false
     },
     //The main genre name and id will be grabbed from the api
